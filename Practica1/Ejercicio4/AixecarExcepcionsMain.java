df141382f72ef281c83403bca78fe1716253a1cd
oscrui <oscrui@dexeus.com>
2020-03-19 17:36:02 +0100

    Terminado el ejercicio 4 de forma secuencial sin encapsulación ni estructura

--- Practica1/Ejercicio4/AixecarExcepcionsMain.java
+++ Practica1/Ejercicio4/AixecarExcepcionsMain.java
@@ -1,5 +1,6 @@
 package Practica1.Ejercicio4;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -9,24 +10,57 @@ import java.util.Scanner;
  * El programa principal farà servir la funció per anar construint una llista de quantitats.  Ha de controlar
  * que en cas que la funció aixequi una excepció (per numero negatiu), s’informi per pantalla a l’usuari de l’error,
  * i en comptes d’afegir el número, s’afegeixi a la llista un element amb el text ‘NULL’.
- * */
+ */
 public class AixecarExcepcionsMain {
 
     static Scanner teclado = new Scanner(System.in);
-    static double[] quantitats;
+    static String[] quantitats;
     static final String NUMERO_NO_PERMITIDO = "NULL";
+    static int cantidadIntroducida;
 
     public static void main(String[] args) {
 
-        System.out.println("De cuantos numeros se compone la lista?");
-        quantitats = new double[teclado.nextInt()];
+        while (true) {
+            try {
+                System.out.println("De cuantos numeros se compone la lista?");
+                quantitats = new String[teclado.nextInt()];
+                System.out.println("El listado se compone de: " + quantitats.length + " valores.");
+                break;
 
-        for (double quantitat : quantitats) {
+            } catch (InputMismatchException e) {
+                System.out.println("El dato debe ser númerico: " + e);
+                teclado.next();
 
-            quantitat = 21;
-            System.out.println(quantitat);
+            } catch (NegativeArraySizeException e) {
+                System.out.println("El programa no puede montar listas de cantidad negativa: " + e);
+            }
         }
 
-    }// cierre del main
+        for (int i = 0; i < quantitats.length; i++) {
 
+            System.out.println("Cantidad num.: " + (i + 1));
+
+            if (teclado.hasNextInt())
+                cantidadIntroducida = teclado.nextInt();
+            else
+                System.out.println("La entrada debe ser númerica");
+            quantitats[i] = NUMERO_NO_PERMITIDO;
+
+            if (cantidadIntroducida < 0) {
+
+                System.out.println("El numero negativo se ha anulado.");
+                quantitats[i] = NUMERO_NO_PERMITIDO;
+
+            } else {
+                quantitats[i] = Integer.toString(cantidadIntroducida);
+            }
+        }
+
+        // 3. Mostramos todos los elementos de la lista
+        System.out.println("Los elementos que ha introducido son:");
+        for (String quantitat : quantitats) {
+
+            System.out.print(quantitat + " ");
+        }
+    }
 }
